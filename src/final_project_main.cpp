6faff40823f1a72f27a35f9157434d95c2d51cfe
Zhiyuxiao <zhiyu_xiao1221@outlook.com>
2020-11-21 22:23:47 +0800

    make hdt and tonemap

--- src/final_project_main.cpp
+++ src/final_project_main.cpp
@@ -95,11 +95,37 @@ void CompareTwoBilateral()
 	rgbBilatIm.write(DATA_DIR "/output/fastBilateralResults/Compare_Bilateral.png");
 	printf("Bilateral took %3.5f seconds\n", timer.elapsed() / 1000.f);
 }
+void testMakeAndToneMap_Room()
+{
 
+	vector<string> filenames;
+	int nImages = 8;
+
+	// load an image sequence
+	vector<FloatImage> imSeq;
+	for (int i = 1; i <= nImages; i++)
+	{
+		ostringstream ss;
+		ss << DATA_DIR "/input/final_project/indoor/scene1/Angle2/sphere_" << i << ".jpg";
+		string filename = ss.str();
+		imSeq.push_back(FloatImage(filename));
+	}
+
+	// generate an hdr image
+	FloatImage hdr = makeHDR(imSeq, 0.1, 0.9);
+	// tone map with bilaterial
+	// tone map with gaussian blur
+	//toneMap(hdr, 20, 1.5).write(DATA_DIR "/output/room2Angle2--tonedHDRsimple-gauss.png");
+	//toneMap(hdr, 100, 1.5).write(DATA_DIR "/output/room2Angle2--tonedHDRsimple-gauss2.png");
+	// tone map with bilaterial
+	//toneMap(hdr, 20, 1.5, true, 0.2).write(DATA_DIR "/output/room2Angle2-tonedHDRsimple-bilateral.png");
+	toneMap(hdr, 100, 3, true, 0.2).write(DATA_DIR "/output/room2Angle2--tonedHDRsimple-bilateral2.png");
+}
 int main()
 {
 	//testPanoramicTrans();
 	//testCompositing();
 	//testFastBilateral();
-	CompareTwoBilateral();
+	//CompareTwoBilateral();
+	testMakeAndToneMap_Room();
 }
